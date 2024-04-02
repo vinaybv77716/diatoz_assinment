ddd0d132945a47dd726b545c6cb719c59d211975
vinaybv77716 <144867641+vinaybv77716@users.noreply.github.com>
2024-04-02 11:15:26 +0530

    Update backend.tf

--- terraorm_code/backend.tf
+++ terraorm_code/backend.tf
@@ -1,11 +1,11 @@
 
-#terraform {
-#  backend "s3" {
-#    bucket = "jms-terraform-backend"
-#    key    = "jmsth_jenkins.tfstate"
-#    region = "ap-south-1"
-#    encrypt = true
+terraform {
+  backend "s3" {
+    bucket = "diatozterraform"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+ #   encrypt = true
 #    dynamodb_table = "terraform-state-lock-dynamo"
-#    }
-#  }
+    }
+  }
 
